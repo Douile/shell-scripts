fc90c6221d927900340bc151d9ced2b16021e7ff
Douile <25043847+Douile@users.noreply.github.com>
2020-11-25 19:03:21 +0000

    Add password generator

---
+++ sources/password.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+generatePassword() {
+  local cChars
+  cChars=${1:-32}
+
+  tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c "$cChars"
+}
+
+checkPassword() {
+  local pwd="$1"
+  local hash
+  hash=$(echo -n "$pwd" | sha1sum | cut -c -40)
+  local expected=${hash#?????}
+  local prefix=${hash%$expected}
+  local match
+  match=$(curl -s "https://api.pwnedpasswords.com/range/${prefix}" | grep -i "^$expected:" || return 1)
+  echo "${match#*:}" | tr -d '\r'
+}
+
+generateAndCheckPassword() {
+  pass="$(generatePassword $1)"
+  echo "$pass"
+  occ="$(checkPassword "$pass")"
+  if [[ -z "$occ" ]]; then
+    echo "No occurences"
+  else
+    echo "$occ occurences"
+  fi
+}
--- sources/random.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rnd() {
-	base64 /dev/urandom | head -c $1
-	echo ''
-}
