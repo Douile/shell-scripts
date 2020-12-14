94458db8e93afcb2f5a155d199be5148a7bed810
Douile <25043847+Douile@users.noreply.github.com>
2020-12-14 13:19:04 +0000

    Add multiple hash formats to integrity

--- sources/integrity.sh
+++ sources/integrity.sh
@@ -3,6 +3,14 @@
 # Generate a HTML5 integrity hash
 # example integrity http://localhost:5000/src/index.js
 function integrity {
-	printf "integrity=\"sha512-%s\"\n" $( wget -qO- $1 | openssl dgst -sha512 -binary | openssl base64 -A )
+  hash="sha512"
+  url="$1"
+  case $1 in
+    sha512|sha384|sha256)
+      hash="$1"
+      url="$2"
+      ;;
+  esac
+	printf "integrity=\"$hash-%s\"\n" $( curl -s $url | openssl dgst -$hash -binary | openssl base64 -A )
 }
-[[ $_ != $0 ]] && echo "Loaded integrity..." || integrity $1
+[[ $_ = $0 ]] && integrity $@
