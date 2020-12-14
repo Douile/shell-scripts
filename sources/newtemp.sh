f731b4ada676d821873270f327a7a616a0f2832c
Douile <25043847+Douile@users.noreply.github.com>
2020-12-14 13:10:26 +0000

    Make closetemp recursable

--- sources/newtemp.sh
+++ sources/newtemp.sh
@@ -2,9 +2,10 @@
 
 # Create a temp directory and cd into
 newtemp() {
+  lastclose=$(command -v closetemp)
   OLDPWD="$PWD"
   tmpdir=$(mktemp -d)
   cd "$tmpdir"
-  alias closetemp="cd $OLDPWD && rm -r $tmpdir; unalias closetemp"
+  alias closetemp="cd $OLDPWD && rm -rf $tmpdir; unalias closetemp; $lastclose"
   echo "Moved to temp directory $tmpdir use closetemp to delete and go back"
 }
