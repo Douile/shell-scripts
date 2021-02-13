0df5956ea01367c5bb6ec8884b15a76c848b5286
Douile <25043847+Douile@users.noreply.github.com>
2021-02-13 04:37:46 +0000

    Update script

--- luks-llvm/installer.sh
+++ luks-llvm/installer.sh
@@ -34,209 +34,142 @@ echo "500MB (fat32 efi partition)"
 echo "Remainder (luks + lvm)"
 fdisk "$DISK"
 
-echo "Setup partitions and mount"
-cat << EOF > /usr/bin/parthelp
-#!/bin/sh
-echo "cryptsetup luksFormat --type=luks1 ${DISK}3"
-echo "cryptsetup open ${DISK}3 cryptlvm"
-echo "pvcreate /dev/mapper/cryptlvm"
-echo "vgcreate Volumes /dev/mapper/cryptlvm"
-echo "lvcreate -L 8G Volumes -n swap"
-echo "lvcreate -l 100%FREE Volumes -n root"
-echo "mkfs.fat -F32 ${DISK}2"
-echo "mkfs.btrfs /dev/Volumes/root"
-echo "mkswap /dev/Volumes/swap"
-echo "== OPTIONAL SUBVOLUMES for snapper ('setupbtrfs ROOT SWAP' for auto) =="
-echo "== Leave mounted when continuing =="
-echo "mount /dev/Volumes/root /mnt"
-echo "cd /mnt"
-echo "btrfs subvolume create @"
-echo "btrfs subvolume create @snapshots"
-echo "btrfs subvolume create @home"
-echo "btrfs subvolume create @logs"
-echo "btrfs subvolume create @pc-cache"
-echo "cd /"
-echo "umount /mnt"
-echo "mount -o compress=zstd,subvol=@ /dev/Volumes/root /mnt"
-echo "mkdir -p /mnt/.snapshots"
-echo "mount -o subvol=@snapshots /dev/Volumes/root/ /mnt/.snapshots"
-echo "mkdir -p /mnt/home"
-echo "mount -o subvol=@home /dev/Volumes/root /mnt/home"
-echo "mkdir -p /mnt/var/log"
-echo "mount -o subvol=@logs /dev/Volumes/root /mnt/var/log"
-echo "mkdir -p /mnt/var/cache/pacman/pkg"
-echo "mount -o subvol=@pc-cache /dev/Volumes/root /mnt/var/cache/pacman/pkg"
-echo "mkdir -p /mnt/tmp"
-echo "mount -t tmpfs tmpfs /mnt/tmp"
-echo "swapon /dev/Volumes/swap"
-echo "mkdir -p /mnt/efi"
-echo "mount ${DISK}2 /mnt/efi"
-EOF
-chmod +x /usr/bin/parthelp
-cat << EOF > /usr/bin/setupbtrfs
-#!/bin/sh
-
-set -eux
-
-ROOT=\$1
-SWAP=\$2
-if [ ! -b \$ROOT ]; then
-  echo "\$ROOT Doesn't exist"
+read -p "Enter main partition (where LLVM will be installed) (e.g. /dev/sda3): " ROOT
+if [ ! -b $ROOT ]; then
+  echo "Device doesn't exist"
   exit 1
 fi
-mount \$ROOT /mnt
-cd /mnt
+
+read -p "Enter swap size (e.g. 8G) (Leave blank for no swap): " SWAP_SIZE
+
+read -p "Enter EFI partition (e.g. /dev/sda2): " EFI
+if [ ! -b $EFI ]; then
+  echo "Device doesn't exist"
+  exit 1
+fi
+
+echo "Seting up partitions..."
+cryptsetup luksFormat --type=luks1 "${ROOT}"
+cryptsetup open ${ROOT} cryptlvm
+pvcreate /dev/mapper/cryptlvm
+vgcreate Volumes /dev/mapper/cryptlvm
+if [ "$SWAP_SIZE" != "" ]; then
+  lvcreate -L "$SWAP_SIZE" Volumes -n swap
+fi
+lvcreate -l 100%FREE Volumes -n root
+
+echo "Formatting drives..."
+mkfs.fat -F32 "$EFI"
+mkfs.btrfs /dev/Volumes/root
+if [ -b /dev/Volumes/swap ]; then
+  mkswap /dev/Volumes/swap
+fi
+mountpoint=$(mktemp -d)
+mount /dev/Volumes/root "$mountpoint"
+cd "$mountpoint"
 btrfs subvolume create @
 btrfs subvolume create @snapshots
 btrfs subvolume create @home
 btrfs subvolume create @logs
 btrfs subvolume create @pc-cache
 cd /
-umount /mnt
-mount -o subvol=@ \$ROOT /mnt
-mkdir -p /mnt/.snapshots
-mount -o subvol=@snapshots \$ROOT /mnt/.snapshots
-mkdir -p /mnt/home
-mount -o subvol=@home \$ROOT /mnt/home
-mkdir -p /mnt/var/log
-mount -o subvol=@logs \$ROOT /mnt/var/log
-mkdir -p /mnt/var/cache/pacman/pkg
-mount -o subvol=@pc-cache \$ROOT /mnt/var/cache/pacman/pkg
-
-if [ -b \$SWAP ]; then
-  swapon \$SWAP
+umount "$mountpoint"
+mount -o subvol=@ /dev/Volumes/root "$mountpoint"
+mkdir -p "$mountpoint/.snapshots"
+mount -o subvol=@snapshots /dev/Volumes/root "$mountpoint/.snapshots"
+mkdir -p "$mountpoint/home"
+mount -o subvol=@home /dev/Volumes/root "$mountpoint/home"
+mkdir -p "$mountpoint/var/log"
+mount -o subvol=@logs /dev/Volumes/root "$mountpoint/var/log"
+mkdir -p "$mountpoint/var/cache/pacman/pkg"
+mount -o subvol=@pc-cache /dev/Volumes/root "$mountpoint/var/cache/pacman/pkg"
+if [ -b /dev/Volumes/swap]; then
+  swapon /dev/Volumes/swap
 fi
+mkdir -p "$mountpoint/efi"
+mount "$EFI" "$mountpoint/efi"
 
-mkdir -p /mnt/efi
-echo "Remember to mount EFI to /mnt/efi"
-EOF
-chmod +x /usr/bin/setupbtrfs
-
-parthelp
-echo "parthelp to repeat above message"
-zsh
-# ...
-# cryptsetup open "{DISK}2" cryptlvm
-# pvcreate /dev/mapper/cryptlvm
-# vgcreate Volumes /dev/mapper/cryptlvm
-# lvcreate -L 8G Volumes -n swap
-# lvcreate -l 100%FREE Volumes -n root
-# mkfs.fat -F32 "${DISK}1"
-# mkfs.btrfs /dev/Volumes/root
-# mkswap /dev/Volumes/swap
-read -p "Enter root device: " ROOT
-if [ ! -b $ROOT ]; then
-  echo "Device doesn't exist"
-  exit 1
-fi
-read -p "Enter swap directory: (blank for no swap) " SWAP
-if [ ! -b $SWAP ] && [ "$swap" != "" ]; then
-  echo "Device doesn't exist"
-  exit 1
-fi
-
-read -p "Enter EFI device: " EFI
-if [ ! -b $EFI ]; then
-  echo "Device doesn't exist"
-  exit 1
-fi
-
-read -p "Enter crypto disk device: " CRYPTODISK
-if [ ! -b $CRYPTODISK ]; then
-  echo "Device doesn't exist"
-  exit 1
-fi
-
-echo "ROOT: $ROOT"
-echo "SWAP: $SWAP"
-echo "EFI: $EFI"
-echo "CRYPTODISK: $CRYPTODISK"
-confirm "Are disks OK? (YES to continue) "  "YES"
-
-echo "Mounting..."
-if mountpoint -q /mnt; then
-  confirm "The root mountpoint (/mnt) is already mounted (YES to continue and skip auto-mounting) " "YES"
-else
-  mount $ROOT /mnt
-  mkdir -p /mnt/boot
-  mount "$EFI" /mnt/boot
-  if [ "$SWAP" != "" ]; then
-    swapon $SWAP
-  fi
-fi
 echo "Installing packages..."
-pacstrap /mnt base linux linux-firmware btrfs-progs base-devel
+pacstrap "$mountpoint" --noconfirm base linux-zen linux-lts linux-firmware btrfs-progs base-devel
 echo "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab
-cat /mnt/etc/fstab
+genfstab -U "$mountpoint" >> $mountpoint/etc/fstab
+less "$mountpoint/etc/fstab"
 confirm "Is fstab ok? (YES to continue) " "YES"
 
 cr() {
-  arch-chroot /mnt $@
+  arch-chroot "$mountpoint" $@
 }
+
+echo "Creating keyfile"
+mkdir -p "$mountpoint/root"
+dd bs=512 count=4 if=/dev/random of="$mountpoint/root/cryptlvm.keyfile" iflag=fullblock
+chmod 000 "$mountpoint/root/cryptlvm.keyfile"
+cr cryptsetup -v luksAddKey "${ROOT}" /root/cryptlvm.keyfile
 
 echo "Configuring time..."
 cr ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 cr hwclock --systohc
 
 echo "Generating locales..."
-sed -i 's/\#\(en_GB.UTF-8 UTF-8\)/\1/' /mnt/etc/locale.gen
+sed -i 's/\#\(en_GB.UTF-8 UTF-8\)/\1/' "$mountpoint/etc/locale.gen"
 cr locale-gen
-echo "LANG=en_GB.UTF-8" > /mnt/etc/locale.conf
-echo "KEYMAP=uk" > /mnt/etc/vconsole.conf
+echo "LANG=en_GB.UTF-8" > $mountpoint/etc/locale.conf
+echo "KEYMAP=uk" > $mountpoint/etc/vconsole.conf
 
 echo "Setting up hostname..."
 read -p "Enter hostname: " HOSTNAME
-echo "$HOSTNAME" > /mnt/etc/hostname
-cat << EOF >> /mnt/etc/hosts
+echo "$HOSTNAME" > $mountpoint/etc/hostname
+cat << EOF >> $mountpoint/etc/hosts
 127.0.0.1 localhost
 ::1       localhost
 127.0.1.1 $HOSTNAME.localdomain  $HOSTNAME
 EOF
 
 echo "Installing packages..."
-cr pacman -S --needed --noconfirm grub lvm2 efibootmgr vim htop man-db dash networkmanager pulseaudio pulseaudio-zeroconf
+cr pacman -S --needed --noconfirm grub lvm2 efibootmgr neovim htop man-db dash networkmanager pulseaudio pulseaudio-zeroconf
 
 echo "Setting up initcpio..."
-# BINARIES("/usr/bin/btrfs")
-sed -i 's/^BINARIES=([^)]*)/BINARIES=("\/usr\/bin\/btrfs")/' /mnt/etc/mkinitcpio.conf
-# HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)
-sed -i 's/^HOOKS=([^)]*)/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)/' /mnt/etc/mkinitcpio.conf
-#COMPRESSION="gzip"
 
-less /mnt/etc/mkinitcpio.conf
+# BINARIES("/usr/bin/btrfs")
+sed -i 's/^BINARIES=([^)]*)/BINARIES=("\/usr\/bin\/btrfs")/' "$mountpoint/etc/mkinitcpio.conf"
+# HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)
+sed -i 's/^HOOKS=([^)]*)/HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt lvm2 filesystems fsck)/' "$mountpoint/etc/mkinitcpio.conf"
+#COMPRESSION="gzip"
+# FILES=("/root/cryptlvm.keyfile")
+sed -i 's/^FILES=([^)]*)/FILES=("\/root\/cryptlvm.keyfile")/' "$mountpoint/etc/mkinitcpio.conf"
+
+less $mountpoint/etc/mkinitcpio.conf
 read -p "Is mkinitcpio.conf ok? (YES to continue, EDIT to edit) " check
 case $check in
 "YES") ;;
-"EDIT") vim /mnt/etc/mkinitcpio.conf ;;
+"EDIT") vim $mountpoint/etc/mkinitcpio.conf ;;
 *) exit 1
 esac
 cr mkinitcpio -P
+chmod 600 $mountpoint/boot/initramfs-linux*
 
 echo "Setting up grub..."
 # Get root UUID
-PARTID=$(blkid -s PARTUUID -o value $CRYPTODISK)
+PARTID=$(blkid -s PARTUUID -o value $ROOT)
 # blkid of /dev/mapper/Volumes/root
 
-if [ "$SWAP" != "" ]; then
-  ESCAPE_SWAP=$(echo "$SWAP" | sed 's/\//\\\//g')
-  sed -i "s/^\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)$\"/\1 resume=${ESCAPE_SWAP}\"/" /mnt/etc/default/grub
+if [ -b /dev/Volumes/swap ]; then
+  sed -i "s/^\(GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*\)$\"/\1 resume=\/dev\/Volumes\/swap\"/" "$mountpoint/etc/default/grub"
 fi
 
-ESCAPE_CRYPT=$(echo "$CRYPTDISK" | sed 's/\//\\\//g')
-sed -i "s/^\(GRUB_CMDLINE_LINUX=\"[^\"]*\)\"/\1cryptdevice=PARTUUID=${PARTID}:cryptlvm root=$ESCAPE_CRYPT\"/" /mnt/etc/default/grub
-sed -i 's/^\(GRUB_PRELOAD_MODULES="[^"]*\)"/\1 lvm"/' /mnt/etc/default/grub
-sed -i 's/^#\(GRUB_ENABLE_CRYPTODISK=\).*/\1y/' /mnt/etc/default/grub
-less /mnt/etc/default/grub
+sed -i "s/^\(GRUB_CMDLINE_LINUX=\"[^\"]*\)\"/\1cryptdevice=PARTUUID=${PARTID}:cryptlvm root=\/dev\/Volumes\/root cryptkey=rootfs:\/root\/cryptlvm.keyfile\"/" "$mountpoint/etc/default/grub"
+sed -i 's/^\(GRUB_PRELOAD_MODULES="[^"]*\)"/\1 lvm"/' "$mountpoint/etc/default/grub"
+sed -i 's/^#\(GRUB_ENABLE_CRYPTODISK=\).*/\1y/' "$mountpoint/etc/default/grub"
+less "$mountpoint/etc/default/grub"
 read -p "Is grub default ok? (YES to continue, EDIT to edit) " check
 case $check in
 "YES") ;;
-"EDIT") vim /mnt/etc/default/grub ;;
+"EDIT") vim "$mountpoint/etc/default/grub" ;;
 *) exit 1
 esac
 
 if confirm "Add custom grub options? (y for yes) " "y"; then
-  cat << EOF >> /mnt/etc/grub.d/40_custom
+  cat << EOF >> $mountpoint/etc/grub.d/40_custom
 menuentry "System shutdown" {
   echo "System shutting down..."
   halt
@@ -253,14 +186,14 @@ fi
 EOF
 fi
 
-cr grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB $ROOT
+cr grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB-arch $ROOT
 cr grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "Set root password"
 cr passwd
 echo "Setting up programs..."
 cr systemctl enable NetworkManager
-rm /mnt/bin/sh
+rm $mountpoint/bin/sh
 cr ln -s /bin/dash /bin/sh
 
 echo "Make any manual changes then exit"
@@ -269,7 +202,7 @@ if cr bash; then
 else
   echo "WARNING: manual shell exited with error"
 fi
-umount -R /mnt
+umount -R "$mountpoint"
 echo "Done, you should reboot the system"
 
 # Notes on setting up x
